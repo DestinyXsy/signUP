568d5dba8ed179605a54381d85a106b397c76be1
羽衣夜市 <oncwnuA9XaSVrWX3iCVTWymKgiWw@git.weixin.qq.com>
2021-08-05 19:47:43 +0800

    修改文件结构

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 生产环境 sourceMap
+  productionSourceMap: false,
   publicPath: process.env.NODE_ENV === 'production' ?
-    './' :
+    '/signUP/dist' :
     '/'
 })
